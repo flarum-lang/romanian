54d495e435442106e9255eda45ac111b71de0f06
block-cat <iuliancernei@gmail.com>
2021-06-04 17:13:02 +0300

    Update

--- extend.php
+++ extend.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the Russian language pack for Flarum.
+ * This file is part of the Romanian language pack for Flarum.
  *
  * For detailed copyright and license information, please view the
  * LICENSE file that was distributed with this source code.
